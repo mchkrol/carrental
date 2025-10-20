dc102cd7cd38e79674e697146634eb54c2d6d165
mchkrol <82917545+mchkrol@users.noreply.github.com>
2025-10-20 20:27:19 +0200

    Update RentalValidatorTest.java

--- carrental/src/test/java/com/michalkrol/carrental/validation/RentalValidatorTest.java
+++ carrental/src/test/java/com/michalkrol/carrental/validation/RentalValidatorTest.java
@@ -1,5 +1,12 @@
 package com.michalkrol.carrental.validation;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 import com.michalkrol.carrental.config.RentalProperties;
 import com.michalkrol.carrental.entity.CarType;
 import com.michalkrol.carrental.entity.Customer;
@@ -13,13 +20,6 @@ import org.mockito.Mockito;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 class RentalValidatorTest {
 
